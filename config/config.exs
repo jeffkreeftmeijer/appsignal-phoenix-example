c1084eff572ac861db9f23213b612731d13e03fb
Jeff Kreeftmeijer <jeff@kreeftmeijer.nl>
2016-10-13 10:00:28 +0200

    Configure AppSignal name and API key in config.exs

--- config/config.exs
+++ config/config.exs
@@ -22,6 +22,10 @@ config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
 
+config :appsignal, :config,
+  name: :appsignal_phoenix_example,
+  push_api_key: "00000000-0000-0000-0000-000000000000"
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env}.exs"
