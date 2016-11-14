94587ab9e524649d698fb7e7b537896c383fc8f9
Jeff Kreeftmeijer <jeff@kreeftmeijer.nl>
2016-11-14 15:17:48 +0100

    Implement chat server as per http://www.phoenixframework.org/docs/channels

--- lib/appsignal_phoenix_example_web/channels/user_socket.ex
+++ lib/appsignal_phoenix_example_web/channels/user_socket.ex
@@ -2,7 +2,11 @@ defmodule AppsignalPhoenixExampleWeb.UserSocket do
   use Phoenix.Socket
 
   ## Channels
-  # channel "room:*", AppsignalPhoenixExampleWeb.RoomChannel
+  channel "room:*", AppsignalPhoenixExampleWeb.RoomChannel
+
+  ## Transports
+  transport(:websocket, Phoenix.Transports.WebSocket)
+  # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
   # be used to verify and authenticate a user. After
