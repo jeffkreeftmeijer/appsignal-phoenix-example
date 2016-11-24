5705aa2ae6733c2a6887862c6d0e0de7ac57d3dc
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2016-11-24 11:51:33 +0100

    Add /exception

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -4,4 +4,8 @@ defmodule AppsignalPhoenixExample.PageController do
   def index(conn, _params) do
     render conn, "index.html"
   end
+
+  def exception(conn, _params) do
+    raise "ecxeption!"
+  end
 end
