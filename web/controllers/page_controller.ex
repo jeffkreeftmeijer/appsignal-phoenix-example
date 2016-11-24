e9dc389957dd1f9a6ea818dd04700ae2d73328d3
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2016-11-24 11:51:33 +0100

    Add /exception

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -4,4 +4,8 @@ defmodule AppsignalPhoenixExample.PageController do
   def index(conn, _params) do
     render conn, "index.html"
   end
+
+  def exception(_conn, _params) do
+    raise "exception!"
+  end
 end
--- web/router.ex
+++ web/router.ex
@@ -17,6 +17,7 @@ defmodule AppsignalPhoenixExample.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+    get "/exception", PageController, :exception
   end
 
   # Other scopes may use custom stacks.
