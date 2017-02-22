aad5d2937bb94a91ba6a120bd95349fd04918583
Tom de Bruijn <tom@tomdebruijn.com>
2017-02-22 14:22:31 +0100

    Add distillery for app builds

--- mix.exs
+++ mix.exs
@@ -38,6 +38,7 @@ defmodule AppsignalPhoenixExample.Mixfile do
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
      {:cowboy, "~> 1.0"},
+     {:distillery, "~> 1.0"},
      {:appsignal, "~> 0.0"}]
   end
 
