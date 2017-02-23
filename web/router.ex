bca261f075639b8b2216d55de9b2e709d56084c3
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2017-02-23 17:33:29 +0100

    Add /slow and /exception

--- web/router.ex
+++ web/router.ex
@@ -17,6 +17,8 @@ defmodule AppsignalPhoenixExample.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+    get "/slow", PageController, :slow
+    get "/exception", PageController, :exception
   end
 
   # Other scopes may use custom stacks.
