a10543388db1a4f166551463eda8eb659270b6e0
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2017-02-23 17:33:29 +0100

    Add /slow and /exception

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -4,4 +4,15 @@ defmodule AppsignalPhoenixExample.PageController do
   def index(conn, _params) do
     render conn, "index.html"
   end
+
+  def slow(conn, _params) do
+    time = :rand.uniform(3000)
+    :timer.sleep(time)
+
+    text conn, "Slept for #{time}ms."
+  end
+
+  def exception(_conn, _params) do
+    raise("Exception!")
+  end
 end
