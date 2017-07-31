7da22880a5d299144a7e3f6b1f29adacd0d00e4e
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2017-07-31 12:34:54 +0200

    Add Query and Backend to RoomChannel

--- web/channels/room_channel.ex
+++ web/channels/room_channel.ex
@@ -1,15 +1,33 @@
 defmodule AppsignalPhoenixExample.RoomChannel do
   use Phoenix.Channel
   use Appsignal.Instrumentation.Decorators
-  alias AppsignalPhoenixExample.{Repo, User}
 
   @decorate channel_action
   def join("room:lobby", _message, socket) do
-    Repo.all(User)
+    Backend.fetch()
     {:ok, socket}
   end
   @decorate channel_action
   def join("room:" <> _private_room_id, _params, _socket) do
     {:error, %{reason: "unauthorized"}}
+  end
+end
+
+defmodule Query do
+  import Ecto.Query
+
+  def for_id(queryable, id) do
+    from m in queryable, where: m.id == ^id
+  end
+end
+
+defmodule Backend do
+  alias AppsignalPhoenixExample.{Repo, User}
+  import Query
+
+  def fetch do
+    User
+    |> for_id(1)
+    |> Repo.all
   end
 end
