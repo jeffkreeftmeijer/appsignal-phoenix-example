b4612f0f18758a63d3f7dd58e6ad00f040d1aff0
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2017-07-31 12:32:31 +0200

    Update appsignal to 1.2.3

--- mix.exs
+++ mix.exs
@@ -38,7 +38,7 @@ defmodule AppsignalPhoenixExample.Mixfile do
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
      {:cowboy, "~> 1.0"},
-     {:appsignal, "~> 1.0.0"}]
+     {:appsignal, "~> 1.0"}]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
