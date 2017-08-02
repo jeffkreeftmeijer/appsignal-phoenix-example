4277e9801fa91b01e8790151c1cb0211c770c99f
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2017-08-02 10:43:49 +0200

    $ mix phx.new appsignal-phoenix-example --app appsignal_phoenix_example

---
+++ lib/appsignal_phoenix_example_web/controllers/page_controller.ex
@@ -0,0 +1,7 @@
+defmodule AppsignalPhoenixExampleWeb.PageController do
+  use AppsignalPhoenixExampleWeb, :controller
+
+  def index(conn, _params) do
+    render conn, "index.html"
+  end
+end
---
+++ lib/appsignal_phoenix_example_web/router.ex
@@ -0,0 +1,26 @@
+defmodule AppsignalPhoenixExampleWeb.Router do
+  use AppsignalPhoenixExampleWeb, :router
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+  end
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/", AppsignalPhoenixExampleWeb do
+    pipe_through :browser # Use the default browser stack
+
+    get "/", PageController, :index
+  end
+
+  # Other scopes may use custom stacks.
+  # scope "/api", AppsignalPhoenixExampleWeb do
+  #   pipe_through :api
+  # end
+end
