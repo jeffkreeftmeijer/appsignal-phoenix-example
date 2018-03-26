7ced95775de5f7d7be7deda7ef5a4082a77f3af1
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2018-03-26 12:06:18 +0200

    Add verk

--- lib/appsignal_phoenix_example/application.ex
+++ lib/appsignal_phoenix_example/application.ex
@@ -14,6 +14,7 @@ defmodule AppsignalPhoenixExample.Application do
       supervisor(AppsignalPhoenixExampleWeb.Endpoint, []),
       # Start your own worker by calling: AppsignalPhoenixExample.Worker.start_link(arg1, arg2, arg3)
       # worker(AppsignalPhoenixExample.Worker, [arg1, arg2, arg3]),
+      supervisor(Verk.Supervisor, [])
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
