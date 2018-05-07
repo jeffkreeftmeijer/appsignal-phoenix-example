652d6f69d69b57bfa60b5aa21e892c6ea21263a8
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2018-05-07 22:29:38 +0930

    Update appsignal to 1.6.0

--- config/appsignal.exs
+++ config/appsignal.exs
@@ -3,4 +3,9 @@ use Mix.Config
 config :appsignal, :config,
   name: "appsignal-phoenix-example",
   push_api_key: "00000000-0000-0000-0000-000000000000",
+  request_headers: ~w(
+    accept accept-charset accept-encoding accept-language cache-control
+    connection content-length path-info range request-method request-uri
+    server-name server-port server-protocol
+  ),
   env: Mix.env
