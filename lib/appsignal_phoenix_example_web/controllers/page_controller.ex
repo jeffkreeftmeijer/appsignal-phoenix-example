7df758d191cb2366e6f6490ffe4b96cc481d0e31
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2018-05-11 14:25:18 +0200

    Udpate appsignal to 1.6.2

--- lib/appsignal_phoenix_example_web/controllers/page_controller.ex
+++ lib/appsignal_phoenix_example_web/controllers/page_controller.ex
@@ -1,7 +1,20 @@
 defmodule AppsignalPhoenixExampleWeb.PageController do
   use AppsignalPhoenixExampleWeb, :controller
 
-  def index(conn, _params) do
+  def index(conn, %{"discard" => _}) do
+    IO.puts "remove!"
+
+    Appsignal.Transaction.set_action("")
+    # self()
+    # |> Appsignal.TransactionRegistry.lookup()
+    # |> IO.inspect
+    # |> Appsignal.TransactionRegistry.remove_transaction()
+    # |> IO.inspect
+
+    render conn, "index.html"
+  end
+
+  def index(conn, _) do
     render conn, "index.html"
   end
 end
--- test/appsignal_phoenix_example/accounts/accounts_test.exs
+++ test/appsignal_phoenix_example/accounts/accounts_test.exs
@@ -62,4 +62,64 @@ defmodule AppsignalPhoenixExample.AccountsTest do
       assert %Ecto.Changeset{} = Accounts.change_user(user)
     end
   end
+
+  describe "users" do
+    alias AppsignalPhoenixExample.Accounts.UserJson
+
+    @valid_attrs %{name: "some name"}
+    @update_attrs %{name: "some updated name"}
+    @invalid_attrs %{name: nil}
+
+    def user_json_fixture(attrs \\ %{}) do
+      {:ok, user_json} =
+        attrs
+        |> Enum.into(@valid_attrs)
+        |> Accounts.create_user_json()
+
+      user_json
+    end
+
+    test "list_users/0 returns all users" do
+      user_json = user_json_fixture()
+      assert Accounts.list_users() == [user_json]
+    end
+
+    test "get_user_json!/1 returns the user_json with given id" do
+      user_json = user_json_fixture()
+      assert Accounts.get_user_json!(user_json.id) == user_json
+    end
+
+    test "create_user_json/1 with valid data creates a user_json" do
+      assert {:ok, %UserJson{} = user_json} = Accounts.create_user_json(@valid_attrs)
+      assert user_json.name == "some name"
+    end
+
+    test "create_user_json/1 with invalid data returns error changeset" do
+      assert {:error, %Ecto.Changeset{}} = Accounts.create_user_json(@invalid_attrs)
+    end
+
+    test "update_user_json/2 with valid data updates the user_json" do
+      user_json = user_json_fixture()
+      assert {:ok, user_json} = Accounts.update_user_json(user_json, @update_attrs)
+      assert %UserJson{} = user_json
+      assert user_json.name == "some updated name"
+    end
+
+    test "update_user_json/2 with invalid data returns error changeset" do
+      user_json = user_json_fixture()
+      assert {:error, %Ecto.Changeset{}} = Accounts.update_user_json(user_json, @invalid_attrs)
+      assert user_json == Accounts.get_user_json!(user_json.id)
+    end
+
+    test "delete_user_json/1 deletes the user_json" do
+      user_json = user_json_fixture()
+      assert {:ok, %UserJson{}} = Accounts.delete_user_json(user_json)
+      assert_raise Ecto.NoResultsError, fn -> Accounts.get_user_json!(user_json.id) end
+    end
+
+    test "change_user_json/1 returns a user_json changeset" do
+      user_json = user_json_fixture()
+      assert %Ecto.Changeset{} = Accounts.change_user_json(user_json)
+    end
+  end
 end
