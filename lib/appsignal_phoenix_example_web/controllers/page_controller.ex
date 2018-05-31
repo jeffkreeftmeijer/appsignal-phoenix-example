f3e81bf3c90cc230fc2562eb0f29717843cf1204
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2018-05-31 16:52:13 +0200

    Remove stray index clause from PageController

--- lib/appsignal_phoenix_example_web/controllers/page_controller.ex
+++ lib/appsignal_phoenix_example_web/controllers/page_controller.ex
@@ -1,19 +1,6 @@
 defmodule AppsignalPhoenixExampleWeb.PageController do
   use AppsignalPhoenixExampleWeb, :controller
 
-  def index(conn, %{"discard" => _}) do
-    IO.puts "remove!"
-
-    Appsignal.Transaction.set_action("")
-    # self()
-    # |> Appsignal.TransactionRegistry.lookup()
-    # |> IO.inspect
-    # |> Appsignal.TransactionRegistry.remove_transaction()
-    # |> IO.inspect
-
-    render conn, "index.html"
-  end
-
   def index(conn, _) do
     render conn, "index.html"
   end
