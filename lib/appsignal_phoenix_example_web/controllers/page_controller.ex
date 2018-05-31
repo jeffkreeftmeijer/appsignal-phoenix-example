1106980c0bdc7a7572a2089823b59e33dda7f4ff
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2018-05-31 16:56:55 +0200

    Add strict param match to index

--- lib/appsignal_phoenix_example_web/controllers/page_controller.ex
+++ lib/appsignal_phoenix_example_web/controllers/page_controller.ex
@@ -1,7 +1,7 @@
 defmodule AppsignalPhoenixExampleWeb.PageController do
   use AppsignalPhoenixExampleWeb, :controller
 
-  def index(conn, _) do
+  def index(conn, %{foo: "bar"}) do
     render conn, "index.html"
   end
 end
