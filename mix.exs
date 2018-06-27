b03e1614154cd31023484426e3e5729a60561735
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2018-06-27 10:25:24 +0200

    Update appsignal to 1.6.6-beta.1

--- mix.exs
+++ mix.exs
@@ -41,7 +41,7 @@ defmodule AppsignalPhoenixExample.Mixfile do
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
       {:cowboy, "~> 1.0"},
-      {:appsignal, "~> 1.0"}
+      {:appsignal, "~> 1.6.6-beta"}
     ]
   end
 
