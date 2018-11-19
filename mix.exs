93e91ed00d00ae180b8fb8d53ff0dc24112301ec
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2018-11-19 11:41:50 +0100

    Update to Cowboy ~> 2.0

--- mix.exs
+++ mix.exs
@@ -40,7 +40,7 @@ defmodule AppsignalPhoenixExample.Mixfile do
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
-      {:plug_cowboy, "~> 1.0"},
+      {:plug_cowboy, "~> 2.0"},
       {:appsignal, "~> 1.0"}
     ]
   end
