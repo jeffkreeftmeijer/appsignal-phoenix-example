d931b09311cefc4880e2149ce1358ff1e336121f
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2019-01-22 16:00:34 +0100

    Add Appsignal template engines to config.exs

--- config/config.exs
+++ config/config.exs
@@ -26,6 +26,10 @@ config :logger, :console,
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
+config :phoenix, :template_engines,
+  eex: Appsignal.Phoenix.Template.EExEngine,
+  exs: Appsignal.Phoenix.Template.ExsEngine
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env()}.exs"
