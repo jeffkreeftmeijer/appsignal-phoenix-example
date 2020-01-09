9a715f5254886d17ce37c07a38b655bba46144dc
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2020-01-09 12:28:44 +0100

    Add instrumentation to LiveView clock

--- lib/appsignal_phoenix_example_web/live/clock_live.ex
+++ lib/appsignal_phoenix_example_web/live/clock_live.ex
@@ -1,17 +1,44 @@
 defmodule AppsignalPhoenixExampleWeb.ClockLive do
   use Phoenix.LiveView
+  import Appsignal.Instrumentation.Helpers, only: [instrument: 4]
 
   def render(assigns) do
     AppsignalPhoenixExampleWeb.ClockView.render("index.html", assigns)
   end
 
   def mount(_session, socket) do
+    transaction =
+      Appsignal.Transaction.generate_id()
+      |> Appsignal.Transaction.start(:liveview)
+      |> Appsignal.Transaction.set_action("AppsignalPhoenixExampleWeb.ClockLive#mount")
+
     :timer.send_interval(1000, self(), :tick)
 
-    {:ok, assign(socket, state: Time.utc_now())}
+    new_socket =
+      instrument(transaction, "query.time", "Getting current time", fn ->
+        assign(socket, state: Time.utc_now())
+      end)
+
+    Appsignal.Transaction.finish(transaction)
+    Appsignal.Transaction.complete(transaction)
+
+    {:ok, new_socket}
   end
 
   def handle_info(:tick, socket) do
-    {:noreply, assign(socket, state: Time.utc_now())}
+    transaction =
+      Appsignal.Transaction.generate_id()
+      |> Appsignal.Transaction.start(:liveview)
+      |> Appsignal.Transaction.set_action("AppsignalPhoenixExampleWeb.ClockLive#tick")
+
+    new_socket =
+      instrument(transaction, "query.time", "Getting current time", fn ->
+        assign(socket, state: Time.utc_now())
+      end)
+
+    Appsignal.Transaction.finish(transaction)
+    Appsignal.Transaction.complete(transaction)
+
+    {:noreply, new_socket}
   end
 end
