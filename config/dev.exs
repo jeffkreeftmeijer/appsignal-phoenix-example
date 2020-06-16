537cfc5b9f44be981fe5eeffe1afff555f8c0374
Tom de Bruijn <tom@tomdebruijn.com>
2020-06-16 10:34:16 +0200

    Broken upgrade setup

--- config/dev.exs
+++ config/dev.exs
@@ -2,8 +2,8 @@ use Mix.Config
 
 # Configure your database
 config :appsignal_phoenix_example, AppsignalPhoenixExample.Repo,
-  username: "postgres",
-  password: "postgres",
+  username: "test",
+  password: "test",
   database: "appsignal_phoenix_example_dev",
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
