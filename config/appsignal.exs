ba8a0880b6f8f191a4403a862c27e3530fbbe87d
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2020-07-10 09:08:54 +0200

    $ mix appsignal.install 00000000-0000-0000-0000-000000000000

---
+++ config/appsignal.exs
@@ -0,0 +1,7 @@
+use Mix.Config
+
+config :appsignal, :config,
+  otp_app: :appsignal_phoenix_example,
+  name: "appsignal_phoenix_example",
+  push_api_key: "00000000-0000-0000-0000-000000000000",
+  env: Mix.env
