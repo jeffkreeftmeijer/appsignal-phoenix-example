1aac796b32018b500b4522f5a44ab9b24e7f8f21
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2020-08-06 08:42:00 +0200

    Add slow event to index

--- lib/appsignal_phoenix_example_web/controllers/page_controller.ex
+++ lib/appsignal_phoenix_example_web/controllers/page_controller.ex
@@ -2,7 +2,14 @@ defmodule AppsignalPhoenixExampleWeb.PageController do
   use AppsignalPhoenixExampleWeb, :controller
 
   def index(conn, _params) do
+    slow()
     render(conn, "index.html")
+  end
+
+  defp slow do
+    Appsignal.instrument("slow", "call.slow", fn span ->
+      :timer.sleep(1000)
+    end)
   end
 
   def exception(conn, _params) do
