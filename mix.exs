7b07f777edd7d59e7af3ba39fc7f6c7e065a21d1
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2020-11-19 08:18:47 +0100

    Update appsignal to 2.0.0

--- mix.exs
+++ mix.exs
@@ -45,7 +45,7 @@ defmodule AppsignalPhoenixExample.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:appsignal_phoenix, "~> 2.0.0-beta.1"}
+      {:appsignal_phoenix, "~> 2.0.0"}
     ]
   end
 
