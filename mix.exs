7936128115fa93e8aee74a22dab38455848b6f06
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2020-11-20 08:04:45 +0100

    Add dialyxir to dependencies

--- mix.exs
+++ mix.exs
@@ -45,7 +45,8 @@ defmodule AppsignalPhoenixExample.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:appsignal_phoenix, "~> 2.0.0"}
+      {:appsignal_phoenix, path: "~/Appsignal/appsignal-elixir-phoenix"},
+      {:dialyxir, "~> 1.0", only: [:dev], runtime: false}
     ]
   end
 
